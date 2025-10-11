68a3478a969f97c7e2153d9f550aa4d5ca7dbe19
Beyzanur <beyzanurdincer502@gmail.com>
2025-10-11 13:26:29 +0300

    Hafta 2: Vigenère Cipher eklendi  | Frontend dropdown ve dinamik endpoint yapısı tamamlandı

--- crypto_project/crypto_app/urls.py
+++ crypto_project/crypto_app/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('encrypt/', views.encrypt, name='encrypt'),
     path('decrypt/', views.decrypt, name='decrypt'),
+    path('vigenere/encrypt/', views.vigenere_encrypt, name='vigenere_encrypt'),
+    path('vigenere/decrypt/', views.vigenere_decrypt, name='vigenere_decrypt'),
+
 ]
